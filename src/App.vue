3e719792fba5ed7e737f82b15157d1ba4eafeb8f
Stanley Seow <stanleyseow@gmail.com>
2022-03-02 20:53:30 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+import Moralis from "moralis";
+const serverUrl = process.env.VUE_APP_MORALIS_SERVER_URL;
+const appId = process.env.VUE_APP_MORALIS_APPLICATION_ID;
+//const contractAddress = "0x77737d90458ef4b0D5253342bD22ceEC406Eba80";
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {
+    Moralis.start({ serverUrl, appId });
+    window.Moralis = Moralis;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+</style>
