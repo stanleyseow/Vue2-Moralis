a2ec7b271c1737bac993796507cee03861f66369
Stanley Seow <stanleyseow@gmail.com>
2022-03-02 22:58:32 +0800

    Completed workshop

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,70 @@
 <template>
-  <div id="app"></div>
+  <div id="app" class="mx-2 my-2">
+    <input v-model="walletAddress" placeholder="Wallet Address" />
+    <button @click="walletQuery(walletAddress)">Submit</button> <br />
+
+    <br />
+    ERC-20 contract :<b>{{ this.contractAddress }}</b>  <br />
+    Name: <b>{{ metaData.name }}</b> <br />
+    Symbol : <b>{{ metaData.symbol }}</b> <br />
+    <br/>
+
+    User Wallet : {{walletAddress}} <br/>
+    <div v-for="item in walletBalance" :key="item.name">
+      Name : {{ item.name}} <br/>
+      SYM: {{ item.symbol}} <br/>
+      Balance: {{ item.balance}} <br/>
+    </div>
+
+  </div>
 </template>
 
 <script>
 import Moralis from "moralis";
 const serverUrl = process.env.VUE_APP_MORALIS_SERVER_URL;
 const appId = process.env.VUE_APP_MORALIS_APPLICATION_ID;
-//const contractAddress = "0x77737d90458ef4b0D5253342bD22ceEC406Eba80";
 export default {
   name: "App",
   components: {},
   data() {
-    return {};
+    return {
+      walletAddress: "",
+      metaData: {},
+      contractAddress: "0x77737d90458ef4b0D5253342bD22ceEC406Eba80",
+      walletBalance: []
+    };
   },
-  methods: {},
+  methods: {
+    async walletQuery(data) {
+      console.log("walletQuery");
+      this.walletAddress = data;
+
+      let options = {
+        chain: "0x4",
+        addresses: this.contractAddress,
+      };
+
+      this.metaData = await Moralis.Web3API.token.getTokenMetadata(options);
+      console.log("metadata: ", this.metaData);
+      this.metaData.name = this.metaData[0].name
+      this.metaData.symbol = this.metaData[0].symbol
+
+      let option2 = {
+        chain: "0x4",
+        address : this.walletAddress
+      }
+      this.walletBalance = await Moralis.Web3API.account.getTokenBalances(option2);
+      console.log("Wallet Balance: ",this.walletBalance )
+
+
+    },
+  },
   computed: {},
   watch: {},
-  mounted() {
+  async mounted() {
     Moralis.start({ serverUrl, appId });
     window.Moralis = Moralis;
+    await Moralis.enableWeb3();
   },
 };
 </script>
